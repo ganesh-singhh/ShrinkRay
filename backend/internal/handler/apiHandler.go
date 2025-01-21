10534a3aae4bd58e12f804ab47baea7e4779e1a3
Ganesh Singh <krganesh1611@gmail.com>
2025-01-21 17:34:36 +0530

    (add): cors setup

--- backend/internal/handler/apiHandler.go
+++ backend/internal/handler/apiHandler.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"time"
+
 	"github.com/Avon11/ShrinkRay/internal/service"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -20,7 +22,14 @@ func NewHandler(service *service.ShortCodeService) *Handler {
 func SetupAPIHandler(rdb *redis.Client) (*gin.Engine, error) {
 	// Initialize the Gin router
 	r := gin.Default()
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           1 * time.Hour,
+	}))
 
 	service := service.NewCodeService(rdb)
 
